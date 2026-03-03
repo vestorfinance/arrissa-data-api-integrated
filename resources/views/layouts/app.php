4d481eceefaf01818ba5f1ba83195b600f9b37a7
Ngonidzashe Jiji <ngonidzashe@arrissadata.com>
2026-03-03 15:37:25 +0200

    Add Arrissa favicon to all pages

--- resources/views/layouts/app.php
+++ resources/views/layouts/app.php
@@ -11,6 +11,7 @@ _Tz8wKpN4::_v();
     <!-- v2026.02.28 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?? 'Arrissa Data API'; ?></title>
+    <link rel="icon" type="image/png" href="/arrisssa-favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
